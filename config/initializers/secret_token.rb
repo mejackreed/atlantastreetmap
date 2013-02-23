c2306d4462eb90c1e84a89abbadab15a57e7265c
Justin Burris <justinbburris@gmail.com>
2013-02-22 21:45:08 -0500

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atlstreets::Application.config.secret_token = '28e972bce1859b3e4817ae55ba6a23fde0c9cb047594d7971d26efc46f9e97fd669eb4869624ddf6332061a8bbcbdbfc7f7df3e9459f5ed618fe6fa5d0cceab4'
