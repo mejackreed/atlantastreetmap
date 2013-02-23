d4d3d2beb762a843ff98b2c1a7cf7067fed9865e
Justin Burris <justinbburris@gmail.com>
2013-02-23 12:51:14 -0500

    Post a link to twitter with the road closure

--- app/controllers/road_closures_controller.rb
+++ app/controllers/road_closures_controller.rb
@@ -5,4 +5,8 @@ class RoadClosuresController < ApplicationController
     headers['Access-Control-Allow-Origin'] = "*"
     render :json => @roads
   end
+
+  def show
+    render 'main/index'
+  end
 end
--- app/models/road_closure.rb
+++ app/models/road_closure.rb
@@ -2,11 +2,15 @@ class RoadClosure < ActiveRecord::Base
   require 'twitter_integration'
   attr_accessible :description, :end_of_closure, :latitude, :longitude, :road_closed, :road_closed_from, :road_closed_to, :start_of_closure, :type
 
-  before_create :tweet_closure
+  after_create :tweet_closure
 
   private
   def tweet_closure
-    message =  "#{road_closed} from #{road_closed_from} to #{road_closed_to}, closed until #{human_time_string}"
+    message =  "#{road_closed} from #{road_closed_from} to #{road_closed_to}, closed until #{human_time_string} "
+    # XXX Don't have time to fix
+    # TODO Fix this to be sane
+    message += Rails.application.routes.url_helpers.road_closure_url(id, host: 'atlantastreetmap.herokuapp.com')
+
     t = TwitterIntegration.init(TwitterAuth.first)
     resp = TwitterIntegration.update(t, message)
 
